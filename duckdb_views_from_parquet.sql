c8e6d7d5f64d07a06ff627c475b42e4525d26e91
Tomas Suarez <83978153+datatomas@users.noreply.github.com>
2025-10-27 23:15:07 -0500

    Create duckdb_views_from_parquet.sql
    feature:create views from a parquet file taking advantage of data lake house capabilites

---
+++ duckdb_views_from_parquet.sql
@@ -0,0 +1,15 @@
+CREATE SCHEMA IF NOT EXISTS bronze_parquet;
+SET schema='bronze_parquet';
+
+-- UFC
+CREATE VIEW IF NOT EXISTS bronze_parquet.ufc_fights AS
+SELECT * FROM read_parquet(
+  '/media/ares/data/db/lake/bronze/ufc_fights/**/*.parquet',
+  union_by_name=true
+);
+
+CREATE VIEW IF NOT EXISTS bronze_parquet.ufc_fighters AS
+SELECT * FROM read_parquet(
+  '/media/ares/data/db/lake/bronze/ufc_fighters/**/*.parquet',
+  union_by_name=true
+);
